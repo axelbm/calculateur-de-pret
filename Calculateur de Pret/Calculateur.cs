27265b2e7ba8dfa46496fc9dc57963fcfc2af20d
Axel Michaud <axelbmichaud@gmail.com>
2018-10-19 16:33:39 -0400

    Initial commit

---
+++ Calculateur de Pret/Calculateur.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculateur_de_Pret
+{
+	class Calculateur
+	{
+		private double prix;
+		public double Prix
+		{
+			get
+			{
+				return prix;
+			}
+			set
+			{
+				if (value >= 0 & value <= 60000)
+				{
+					prix = Math.Round(value, 2);
+				}
+			}
+		}
+
+		private double echange = 0;
+		public double Echange
+		{
+			get { return echange; }
+
+
+			set
+			{
+				if (value >= 0 & value <= 60000)
+				{
+					echange = Math.Round(value, 2);
+				}
+			}
+		}
+
+		private double solde = 0;
+		public double Solde
+		{
+			get { return solde; }
+
+
+			set
+			{
+				if (value >= 0 & value <= 60000)
+				{
+					solde = Math.Round(value, 2);
+				}
+			}
+		}
+
+		private double mdf = 0;
+		public double Mdf
+		{
+			get { return mdf; }
+
+
+			set
+			{
+				if (value >= 0 & value <= 60000)
+				{
+					mdf = Math.Round(value, 2);
+				}
+			}
+		}
+
+		private int duree = 0;
+		public int Duree
+		{
+			get { return duree; }
+
+
+			set
+			{
+				if (value >= 0 & value <= 96)
+				{
+					duree = value/6*6;
+				}
+			}
+		}
+
+		private double tvp = 0;
+		public double Tvp
+		{
+			get { return tvp; }
+
+
+			set
+			{
+				if (value >= 0 & value <= 15)
+				{
+					tvp = Math.Round(value, 2);
+				}
+			}
+		}
+
+		private double taux = 0;
+		public double Taux
+		{
+			get { return taux; }
+
+
+			set
+			{
+				if (value >= 0 & value <= 15)
+				{
+					taux = Math.Round(value, 2);
+				}
+			}
+		}
+
+		//    public Calculateur()
+		//    {
+		//        
+		//    }
+	}
+}
--- Calculateur de Pret/MainWindow.xaml.cs
+++ Calculateur de Pret/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,11 +19,60 @@ namespace Calculateur_de_Pret
 	/// <summary>
 	/// Logique d'interaction pour MainWindow.xaml
 	/// </summary>
+
+
 	public partial class MainWindow : Window
 	{
+		private Calculateur Calc;
+		public static MainWindow Main;
+
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			ShowVersement();
+
+			Calc = new Calculateur();
+
+			Main = this;
+			Calc.Prix = 21;
+
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			ShowVersement();
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			ShowMax();
+		}
+
+		private void ShowVersement()
+		{
+			this.CalVersement.Visibility = Visibility.Visible;
+			this.CalMax.Visibility = Visibility.Hidden;
+
+			this.BtnVersement.Background = Brushes.Red;
+			this.BtnMax.Background = Brushes.White;
+		}
+
+		private void ShowMax()
+		{
+			this.CalVersement.Visibility = Visibility.Hidden;
+			this.CalMax.Visibility = Visibility.Visible;
+
+			this.BtnVersement.Background = Brushes.White;
+			this.BtnMax.Background = Brushes.Red;
+		}
+
+
+		private void IsTextAllowed(object sender, TextCompositionEventArgs e)
+		{
+			Regex regex = new Regex("[^0-9]+");
+			e.Handled = regex.IsMatch(e.Text);
 		}
 	}
 }
